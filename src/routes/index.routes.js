1e9e765daef6bba083c3a3f0b1d3fff0585346df
marimber123 <74202934+marimber123@users.noreply.github.com>
2021-05-25 23:33:19 -0500

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req,res)=>{
-    res.send('Bienvenido ctm GAAA')
+    res.send('Bienvenido')
 })
 
 export default router
